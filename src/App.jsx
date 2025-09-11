371d592de408d62253bbae85c1ce8ab3c198d146
Joya322 <jannatferdoush1212@gmail.com>
2025-09-11 11:41:42 +0600

    SelectionToggler component done

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import Banner from "./components/Header/Banner";
 import NavBar from "./components/Header/NavBar";
+import SelectionToggler from "./components/Main/SelectionToggler/SelectionToggler";
 
 function App() {
   return (
     <>
       <NavBar />
-      <Banner/>
+      <Banner />
+      <SelectionToggler/>
     </>
   );
 }
---
+++ src/components/Main/SelectionToggler/SelectionToggler.jsx
@@ -0,0 +1,17 @@
+const SelectionToggler = () => {
+  return (
+    <div className="w-11/12 mx-auto mt-16 mb-10 flex items-center justify-between ">
+      <h3 className="font-bold text-3xl">Available Players</h3>
+      <div className="border border-[rgba(19,19,19,0.1)] rounded-xl">
+        <button className="font-bold px-5 py-3 bg-[rgba(231,254,41,1)] rounded-l-xl">
+          Available
+        </button>
+        <button className="text-black opacity-60 px-5 py-3 rounded-b-xl">
+          Selected (0)
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default SelectionToggler;
