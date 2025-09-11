b9da428630f418f2ad5e078c15b73ae14ff0e9db
Joya322 <jannatferdoush1212@gmail.com>
2025-09-11 14:38:19 +0600

    added add more player button in the selected players section

--- src/components/Main/Cards/Cards.jsx
+++ src/components/Main/Cards/Cards.jsx
@@ -7,17 +7,24 @@ const Cards = ({ cricketers }) => {
   return (
     <div className="w-11/12 mx-auto py-5">
       {/* all players */}
-      <div className="grid grid-cols-3 gap-6">
+      {/* <div className="grid grid-cols-3 gap-6">
         {cricketers.map((cricketer, i) => (
           <Card cricketer={cricketer} key={i} />
         ))}
-      </div>
+      </div> */}
 
       {/* selected players */}
-      <div className="flex flex-col justify-center gap-6 hidden">
+      <div className="flex flex-col justify-center gap-6 ">
         {cricketers.map((cricketer, i) => (
-          <SelectedCricketers key={i} cricketer={cricketer} />)
-        )}
+          <SelectedCricketers key={i} cricketer={cricketer} />
+        ))}
+
+        {/* add more button */}
+        <div className="border border-black rounded-xl bg-[rgba(255,255,255,.05)] self-start">
+          <button className="font-bold px-5 py-3 bg-[rgba(231,254,41,1)] rounded-xl m-2 text-base">
+            Add More Player
+          </button>
+        </div>
       </div>
     </div>
   );
--- src/components/Main/Selected/SelectedCricketers.jsx
+++ src/components/Main/Selected/SelectedCricketers.jsx
@@ -1,5 +1,5 @@
-const SelectedCricketers = ({ cricketer }) =>{
-    const { name, image, batting_bowling_type } = cricketer;
+const SelectedCricketers = ({ cricketer }) => {
+  const { name, image, batting_bowling_type } = cricketer;
   return (
     <div className="flex justify-between items-center p-6 border border-black/10 rounded-2xl">
       <div className="flex justify-center items-center gap-6">
@@ -10,8 +10,8 @@ const SelectedCricketers = ({ cricketer }) =>{
         />
 
         <div className="flex flex-col justify-center gap-2">
-            <h4 className="text-2xl font-semibold">{name}</h4>
-            <p className="opacity-60">{batting_bowling_type.split('/')[0]}</p>
+          <h4 className="text-2xl font-semibold">{name}</h4>
+          <p className="opacity-60">{batting_bowling_type.split("/")[0]}</p>
         </div>
       </div>
       <div>
